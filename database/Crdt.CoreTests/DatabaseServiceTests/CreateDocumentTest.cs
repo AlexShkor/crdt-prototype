522063765465d4ab02d23ceb4a0f5035069d275c
Alex Shkor <alex.shkor@gmail.com>
2016-08-08 13:38:37 +0300

    structure changes

---
+++ database/Crdt.CoreTests/DatabaseServiceTests/CreateDocumentTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crdt.CoreTests
+{
+    [TestClass]
+    public class CreateDocumentTest
+    {
+        [TestMethod]
+        public void it_should_add_document_to_storage()
+        {
+        }
+    }
+}
